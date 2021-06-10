167cb46ef969d8a20d1361100461fa75e4a522e5
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-10 12:50:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import HelloWorldList from './HelloWorldList';
-
-const App = () => {
-  return (
-  <div className="App">
-  <HelloWorldList />
-  </div>
-  );
-};
-
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>W3Adda - Hello Wolrd!</h1>
+      </div>
+    );
+  }
+}
+ 
 export default App;
